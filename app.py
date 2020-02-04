147032981d37714c8141b77df961023e6166d007
Dylan Dmitri Gray <d.dylan.g@gmail.com>
2020-02-03 17:35:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return "<a href="https://slack.com/oauth/v2/authorize?client_id=331836816675.936989785591&scope=incoming-webhook"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"></a>"
